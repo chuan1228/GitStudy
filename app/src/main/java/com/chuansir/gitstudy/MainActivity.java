5ad899d8118d240b05d9462b57a3b53a80b3cdd0
dongxiaochuan <dongxiaochuan@feinno.com>
2019-12-13 12:18:05 +0800

    add feature-1 on feature-1 branch

--- app/src/main/java/com/chuansir/gitstudy/MainActivity.java
+++ app/src/main/java/com/chuansir/gitstudy/MainActivity.java
@@ -3,6 +3,8 @@ package com.chuansir.gitstudy;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+
+    /**
+     * feature-1
+     * display HelloWorld
+     *
+     */
+    public void feature1() {
+
+        Log.e(MainActivity.class.getSimpleName(),"HelloWorld!");
+    }
+
 }
